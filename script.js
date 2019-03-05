65a5931c57ed4ed2f1fef61ba9418189fe39852d
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-03-05 09:03:18 +0100

    Add footer and contact

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const tweetElement = document.querySelector('.tweet')
 
 const randomIndex = length => Math.floor(Math.random() * length)
 
+let tweet = null
+
 async function run() {
   const tweetsResponse = await fetch('data/tweets.json')
   const tweets = await tweetsResponse.json()
   const filteredTweets = tweets.filter(tweet => !tweet.excluded)
   const randomTweet = filteredTweets[randomIndex(filteredTweets.length)]
 
+  tweet = randomTweet
   tweetElement.innerText = randomTweet.text
 }
 
 run()
+
+const reportLink = document.getElementById('report')
+reportLink.addEventListener('click', function(event) {
+  this.href = `report.html?id=${tweet.id}`
+})
